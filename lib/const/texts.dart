e0eeb39c252eeef0fa372c0bf108ed5d033913bc
Emir <emircancskn55@gmail.com>
2023-08-19 16:16:00 +0300

    aa

--- lib/const/texts.dart
+++ lib/const/texts.dart
@@ -1,3 +1,4 @@
 const String welcomedoctorjpg = 'assets/welcome.jpg';
 const String welcomesText = 'Sana Yakin Bir Doktor Bul';
 const String welcomeSbuttonText = 'Başla';
+const String category = 'Kategori';
